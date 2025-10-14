047de58f20e105922135069be9d6744c1eacccfc
A709\dam1 <adrigeada@gmail.com>
2025-10-14 17:21:01 +0200

    Bateria Ejercicios

--- Programacion-ud2/src/main/java/org/example/Ejercicio2.java
+++ Programacion-ud2/src/main/java/org/example/Ejercicio2.java
@@ -1,11 +1,27 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Ejercicio2 {
     static void main() {
-        
+        Scanner teclado = new Scanner(System.in);
+        //EJERCICIO 2. Desarrolla un programa que permita leer 2 cadenas
+        // de caracteres (palabras) diferentes y nos diga cual es la más larga.
 
+        System.out.println("Escribe dos palabras y te diremos cual es más larga");
+        String palabra1 = teclado.nextLine();
+        String palabra2 = teclado.nextLine();
 
+        int longitud1 = palabra1.length();
+        int longitud2 = palabra2.length();
 
+        if (longitud1 > longitud2){
+            System.out.println("La primera palabra es mayor");
+        }else {
+            System.out.println("La segunda palabra es mayor");
+        }
 
+        System.out.println(longitud1);
+        System.out.println(longitud2);
     }
 }
--- Programacion-ud2/src/main/java/org/example/Ejercicio3.java
+++ Programacion-ud2/src/main/java/org/example/Ejercicio3.java
@@ -1,4 +1,22 @@
 package org.example;
 
 public class Ejercicio3 {
+    static void main() {
+        //EJERCICIO 3. Programa que quite a una variable numérica N sus m últimas cifras.
+        //Por ejemplo, si N = 123456 y m = 2, el nuevo valor de N será 1234.
+
+        final int N = 123456;
+        final int m = 2;
+
+        String numero_a_string = Integer.toString(N);
+        System.out.println(numero_a_string);
+
+        String trozo = numero_a_string.substring(m);
+        System.out.println(trozo);
+
+
+
+
+
+    }
 }
