6fa63552052f206418b0217f4d990de4a968ce25
A709\dam1 <adrigeada@gmail.com>
2025-10-17 19:30:08 +0200

    Ejemplos SWITCH

---
+++ Programacion-ud2/src/main/java/org/example/swicth_ejemplo.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class swicth_ejemplo {
+    static void main() {
+        int numero =  1;
+        char caracter = 'A';
+        String palabra = "Patricia";
+
+        switch (palabra){
+            case "Raul":
+                System.out.println("El alumno es raul");
+                break;  //hay que poner break porque sino hace los siguientes pasos
+            case "Patricia":
+                System.out.println("La profe es patricia");
+                break;
+            default:
+                System.out.println("No se el nombre");
+                break;
+
+        }
+
+        switch (numero){
+            case 1:
+                System.out.println("el numero es 1");
+                break;
+            case 2:
+                System.out.println("el numero es 2");
+                break;
+            default:
+                System.out.println("no es ni 1 ni 2");
+                break;
+                
+        }
+
+
+    }
+}
