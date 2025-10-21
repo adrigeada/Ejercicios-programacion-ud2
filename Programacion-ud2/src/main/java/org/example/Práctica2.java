c9fb91df6f77e7df41f2988764ecb2b03218e78c
A709\dam1 <adrigeada@gmail.com>
2025-10-21 17:21:11 +0200

    Practica 2. Calculadora

---
+++ Programacion-ud2/src/main/java/org/example/Práctica2.java
@@ -0,0 +1,91 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Práctica2 {
+    static void main() {
+        Scanner teclado = new Scanner(System.in);
+
+        double num1 = 0;
+        double resultado = 0;
+        double num2 = 0;
+
+        System.out.println("Bienvenido a la calculadora rápida");
+        System.out.println("Introduce el primer número");
+
+        if (teclado.hasNextDouble()){
+            num1 = teclado.nextDouble();
+        }else {
+            System.out.println("El formato tiene que ser numérico");
+            return;
+        }
+
+        System.out.println("-----------------");
+        System.out.println("[+]  -> Sumar");
+        System.out.println("[-]  -> Restar");
+        System.out.println("[x]  -> Multiplicar");
+        System.out.println("[/]  -> Dividir");
+        System.out.println("[R]  -> Raíz cuadrada");
+        System.out.println("-----------------");
+        System.out.println("Elige una operación");
+
+        String operador = teclado.next();
+        String operador_min = operador.toLowerCase();
+        boolean raiz = operador_min.equals("r");
+
+        if (!raiz) {
+            System.out.println("Introduce el segundo número");
+            try {
+                num2 = teclado.nextDouble();
+            }catch (Exception e){
+                System.out.println("El formato tiene que ser numérico");
+            }
+        }
+
+        switch (operador_min){
+            case "+":
+                resultado = num1+num2;
+                System.out.println(resultado);
+                break;
+
+            case "-":
+                resultado = num1-num2;
+                System.out.println(resultado);
+                break;
+
+            case "x":
+                resultado = num1*num2;
+                System.out.println(resultado);
+                break;
+
+            case "/":
+                if (num2 == 0){
+                    System.out.println("No se puede dividir entre 0");
+                }else {
+                    resultado = num1/num2;
+                    System.out.println(resultado);
+                }
+                break;
+
+            case "r":
+                if (num1 < 0){
+                    System.out.println("Número imaginario");
+                }else {
+                    resultado = Math.sqrt(num1);
+                    System.out.println(resultado);
+                }
+                break;
+
+            default:
+                System.out.println("No has elegido un operador vállido");
+                break;
+        }
+
+
+
+
+
+
+
+    }
+}
