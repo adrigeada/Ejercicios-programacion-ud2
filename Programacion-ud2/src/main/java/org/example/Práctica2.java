46ea2034a033958c16140912afbbefda18637c9c
A709\dam1 <adrigeada@gmail.com>
2025-10-21 19:57:58 +0200

    Práctica 2. Calculadora simple

--- Programacion-ud2/src/main/java/org/example/Práctica2.java
+++ Programacion-ud2/src/main/java/org/example/Práctica2.java
@@ -29,20 +29,25 @@ public class Práctica2 {
         System.out.println("-----------------");
         System.out.println("Elige una operación");
 
+        //Leemos el operador y lo pasamos a minúsculas
         String operador = teclado.next();
         String operador_min = operador.toLowerCase();
+        //Creamos un booleano que diga true si el operador es r o R, false otra cosa
         boolean raiz = operador_min.equals("r");
 
-        if (!raiz) { //si es una r se pide y se lee el segundo número
+        //si no es una r se pide y se lee el segundo número
+        if (!raiz) {
             System.out.println("Introduce el segundo número");
             try {
                 num2 = teclado.nextDouble();
             }catch (Exception e){
                 System.out.println("El formato tiene que ser numérico");
+                return;
             }
         }
-        //si no es una r se sale del if y sigue el programa
+        //si es una r se sale del if y sigue el programa
 
+        //Usamos operador_min porque está pasado a minúsculas y no importará si la x y la r están en mayúsuclas
         switch (operador_min){
             case "+":
                 resultado = num1+num2;
@@ -60,7 +65,7 @@ public class Práctica2 {
                 break;
 
             case "/":
-                if (num2 == 0){
+                if (num2 == 0){ //No se puede dividir entre 0, asi que hacemos un control por si el segundo numero es 0
                     System.out.println("No se puede dividir entre 0");
                 }else {
                     resultado = num1/num2;
