8cad489e41a3da282dd891aa3462c159e54819d4
A709\dam1 <adrigeada@gmail.com>
2025-10-22 21:11:00 +0200

    Ejercicio 5. Batería Ejercicios

--- Programacion-ud2/src/main/java/org/example/Ejercicio5.java
+++ Programacion-ud2/src/main/java/org/example/Ejercicio5.java
@@ -1,7 +1,34 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Ejercicio5 {
     static void main() {
+        Scanner teclado = new Scanner(System.in);
+        int contador = 0;
+        final int intentos = 10;
+        boolean contiene = true;
+
+        System.out.println("Jugador 1, escribe una frase");
+        String frase = teclado.nextLine();
+
+
+        do {
+            System.out.println("Jugador 2, escribe una palabra");
+            String palabra = teclado.nextLine();
+
+            contiene = frase.contains(palabra);
+            contador++;
+            System.out.println("Te quedan "+(intentos - contador)+ " intentos");
+        }while (!contiene && contador < intentos);
+
+        if (contador < intentos){
+            System.out.println("ganaste");
+        }else {
+            System.out.println("perdiste");
+        }
+
+
 
     }
 }
