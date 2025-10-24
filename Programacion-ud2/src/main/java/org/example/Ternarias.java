68354281b9c3415d391e2b7aa085a0e2bfe91053
A709\dam1 <adrigeada@gmail.com>
2025-10-24 17:48:45 +0200

    Ternarias

--- Programacion-ud2/src/main/java/org/example/Ternarias.java
+++ Programacion-ud2/src/main/java/org/example/Ternarias.java
@@ -2,14 +2,21 @@ package org.example;
 
 public class Ternarias {
     static void main() {
-        int a = 2, b = 3, c = 4;
+//        int a = 2, b = 3, c = 4;
+//
+//       int resultado = c-- == 4 && a > b ? a += b :
+//                a++ > 0 && c == b ? a += c :
+//                        b == c ? a++ : a--;
+//
+//        System.out.println(resultado);
 
-       int resultado = c-- == 4 && a > b ? a += b :
-                a++ > 0 && c == b ? a += c :
-                        b == c ? a++ : a--;
+        int a = 5;
+        int b = 8;
+        int c = 10;
 
+        int resultado = a++ == 6 && b-- == 8 ? c + a++ == 10 ? c-- : a++ : a + c++  ;
         System.out.println(resultado);
-
+        System.out.println(c);
 
 
 
