12d4caa473520a21593d10dfe1b347fc0b8838c3
DESKTOP-DUN703Q\super <adrigeada@gmail.com>
2025-10-26 15:41:03 +0100

    Practica 3. ISBN

--- Programacion-ud2/src/main/java/org/example/Practica3_1.java
+++ Programacion-ud2/src/main/java/org/example/Practica3_1.java
@@ -32,6 +32,8 @@ public class Practica3_1 {
         System.out.println("[X]    Salir");
         System.out.println("------------");
 
+        //El modo para salir no lo he puesto dentro del switch case, porque sino siempre te iba a pedir que escribas el isbn aunque quieras salir.
+        //Esta parte te hace salir del programa si al elegir modo escribe x o X. Pero si pone algo que no sea uno de los modos te sigue pidiendo el isbn. No se como arreglar eso :/
         modo = teclado.nextLine();
         boolean modox = modo.equalsIgnoreCase("x");
         if (modox){
@@ -40,39 +42,40 @@ public class Practica3_1 {
         }
 
         System.out.println("Introduce ISBN");
-
         isbn = teclado.nextLine();
         longitud = isbn.length();
 
+        //Controlamos que el isbn tenga una longitud de 10 caracteres. Si no, se sale del programa
         if (longitud != 10){
             System.out.println("El isbn tienen que ser 10 caracteres");
             return;
         }
 
+        //Si el isbn tiene una interrogación, la interrogación se va a cambiar por un 0. Y a partir de ese momento se usará el isbn con la sustitución por 0 en el resto del programa
         boolean isbninterrogacion = isbn.contains("?");
         if (isbninterrogacion){
 
             int posicion_interrogacion= isbn.indexOf("?");
 
-            multinterrgogacion = 10 - posicion_interrogacion; //con esto calculo por que número hay que multiplicar al numero que estaría en la posicion de la interrogacion
+            multinterrgogacion = 10 - posicion_interrogacion; //con esto calculo por que número hay que multiplicar al numero que estaría en la posicion de la interrogacion. Lo usaremos mas tarde para encontrar el numero por el que se intercambia la ?
             //Voy a reemplazar la interrogacion por 0. Esto me dará el resultado de sumar todas las multiplicaciones menos la del hueco de la interrogacion.
             isbnarreglo = isbn.replace("?", "0");
             isbn = isbnarreglo;
         }
 
-        for (int i = 0,x=longitud; i<=8 ; i++,x--){
+        for (int i = 0,x=longitud; i<=8 ; i++,x--){ //Hola Patri, no se si se pueden meter 2 variables diferentes en el for porque no lo has explicado, pero es lo único que se me ha ocurrido y parece que funciona :/.No es chatgpt, se me ha ocurrido a mi
 
-            num = isbn.charAt(i);
+            num = isbn.charAt(i); //coge el caracter en la posicion i, cada vuelta la i sube una posicion, empieza en 0. Llego hasta el 8 porque en la posicion 9 puede haber una x que se controla fuera del for
 
             try {
-                numint = Integer.parseInt(String.valueOf(num));
+                numint = Integer.parseInt(String.valueOf(num));//el caracter de la posicion i lo transforma a int para poder hacer operaciones con él. Si habia una ?, esta se ha cambiado por un 0 asi que no da error
             }catch (Exception e){
                 System.out.println("En el ISBN no pueden haber letras");
                 return;
             }
 
-            multiplicaciones = numint*x;
-            resultado += multiplicaciones;
+            multiplicaciones = numint*x; //x es la longitud del isbn (10), el numero sacado antes se multiplica por x en la primera vuelta, cada vuelta se resta 1 a x
+            resultado += multiplicaciones; //el resultado es la suma de los resultados que dan las multiplicaciones
         }
 
         //Voy a controlar que el ultimo dígito del isbn puede ser una X. Entonces la x se sustituiría por 10. Se podría hacer con un replace de x por 10 y luego pasarlo a int, pero como el ultimo digito se multiplica por 1, solo lo sumo al resultado anterior.
@@ -91,12 +94,12 @@ public class Practica3_1 {
                 return;
             }
 
-            resultadofinal = resultado + digitofinal_int;
+            resultadofinal = resultado + digitofinal_int; //Se suma el ultimo digito a la suma de las multiplicaciones anteriores
         }
 
-        switch (modo.toLowerCase()){
+        switch (modo){
 
-
+            //En el modo 1 probamos que si el resto de dividir el resultado final es 0, el isbn es válido, si es diferente a 0, el isbn no es valido
             case "1":
 
                 System.out.println(resultadofinal);
@@ -108,11 +111,13 @@ public class Practica3_1 {
 
                 break;
 
+                //En el caso 2 hacemos un bucle para encontrar el numero por el que se tiene que multiplicar para que el resto de 0
             case "2":
 
-                for (r=0 ; r<=9 ; r++){
+                for (r=0 ; r<=9 ; r++){ //probamos con numeros del 0 al 9
 
-                    if ((resultadofinal + multinterrgogacion*r)%11 == 0){
+                    //En este caso el resultado final es al que se le ha sustituido la ? por 0. Como hemos encontrado el hueco donde estaba la ? sabemos por que numero tenemos que multiplicar el numero r que estamos buscando
+                    if ((resultadofinal + multinterrgogacion*r)%11 == 0){  //Cuando el resto de esta operación de 0, salta el mensaje
                         System.out.println("El dígito que falta es " +r);
                     }
                     System.out.println(r);
