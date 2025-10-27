2a0090c25e7b3655094edaeb8579b5d29b09c2b4
DESKTOP-MVTPKEL\Adrian Geada <adrigeada@gmail.com>
2025-10-27 13:12:23 +0100

    Bateria examen

--- Programacion-ud2/src/main/java/org/example/Bateria3/Ejercicio7.java
+++ Programacion-ud2/src/main/java/org/example/Bateria3/Ejercicio7.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class Ejercicio7 {
     static void main() {
         Scanner teclado = new Scanner(System.in);
+        //Desarrolla un programa en Java que permita al usuario generar una secuencia de números
+        //en la que cada número es la suma de los dos anteriores, comenzando desde 0 y 1.
+        //Si el usuario introduce un 5, la salida debe ser:
+        //
+        //Secuencia de Números: 0 1 1 2 3
+        //
+        //Si el usuario introduce un 10, la salida debe ser:
+        //Secuencia de Números: 0 1 1 2 3 5 8 13 21 34
 
         int secuencia = 0;
         int num1 = 0;
@@ -18,7 +26,7 @@ public class Ejercicio7 {
 
             System.out.println(num1);
 
-            resultado = num1+num2;  
+            resultado = num1+num2;
             num1 = num2;
             num2 = resultado;
 
