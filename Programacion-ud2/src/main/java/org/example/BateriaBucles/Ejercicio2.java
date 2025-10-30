4120c692185447f214e1f17685a119ea8c0f413e
DESKTOP-MVTPKEL\Adrian Geada <adrigeada@gmail.com>
2025-10-30 11:42:14 +0100

    Bateria Bucles

--- Programacion-ud2/src/main/java/org/example/BateriaBucles/Ejercicio2.java
+++ Programacion-ud2/src/main/java/org/example/BateriaBucles/Ejercicio2.java
@@ -3,7 +3,7 @@ package org.example.BateriaBucles;
 import java.util.Scanner;
 
 public class Ejercicio2 {
-    static void main() {
+    static void main(String[] args) {
 
         //Imprime los números del N (introducido por el usuario) al 1 en orden descendente.
         Scanner teclado = new Scanner(System.in);
@@ -12,8 +12,22 @@ public class Ejercicio2 {
         int n = teclado.nextInt();
 
        for (int i=n; i>0 ; i--){
-           System.out.println(i);
+           System.out.println(i + "");
 
        }
+
+//       while (n<=100){
+//           System.out.println(n);
+//           n += 2;
+//       }
+
+//        for (int i=0; i<=100 ; i +=2){
+//            System.out.println(i + "");
+//
+//        }
+
+//        for (;;){
+//            System.out.println("a");
+//        }
     }
 }
